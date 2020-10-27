f2c9b3d4e29d917b507169bfb00aacb9b393c650
Meghan Hein <meghan.hein@gmail.com>
2020-10-27 14:17:41 -0700

    add helper function to generate random array

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
-import 'App.scss';
+import React, { useState } from 'react';
+import Questions from './Questions'
+import './App.scss';
 
 export default function App() {
+  const [splashPage, setSplashPage] = useState(true)
+
+  function handleSplash() {
+    splashPage ? setSplashPage(false) : setSplashPage(true)
+  }
+
   return (
     <div className="App">
-      <header>TANDEM QUIZ APP</header>
+      {splashPage && 
+        <div>
+          <h1>Ready to test your Tandem smarts??</h1>
+          <button onClick={handleSplash}>Click here get quizzy!</button>
+        </div>
+      }
+      {!splashPage &&
+        <Questions/>
+      }
     </div>
   );
 }
---
+++ src/helpers/generators.js
@@ -0,0 +1,9 @@
+// Loops through an array and prints a new randomly ordered array of a specified length.
+export function generateRandomArr(arr, newLength) {
+  const resultsArr = [];
+  for (let i = 0; i < newLength; i++) {
+    const newElm = arr[Math.floor(Math.random() * arr.length)];
+    resultsArr.includes(newElm) ? newLength += 1 : resultsArr.push(newElm);
+  }
+  return resultsArr;
+};
