24f54da64fbb5c4b1ab65aa90d1de530bcf49be3
Meghan Hein <meghan.hein@gmail.com>
2020-10-28 11:06:26 -0700

    add filler percentage

--- src/components/Question/ProgressBar.js
+++ src/components/Question/ProgressBar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useStateContext } from 'context/Provider';
+import './ProgressBar.scss'
 
 export default function ProgressBar() {
   const { currentQ } = useStateContext()
   return (
-    <div>
-      
+    <div className='progress-bar'>
+      <div className='filler' style={{width: `${(currentQ + 1) * 10}%`}}/>
+      <p>{currentQ + 1}/10</p>
     </div>
   )
 }
