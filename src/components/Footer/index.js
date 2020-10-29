c4393640dc47318f72db6758283aec4fef74d521
Meghan Hein <meghan.hein@gmail.com>
2020-10-29 00:51:29 -0700

    add footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './style.scss'
 
 export default function Footer() {
   return (
     <footer>
-      <p>Thanks for playing!</p>
+      <p>Created with <FontAwesomeIcon icon={faHeart}/> <a href='https://resume.creddle.io/resume/hq343p8820e' target='_blank'>by Meghan Hein</a></p>
+      <a href='https://github.com/meghein/tandem-quizzes' target='_blank'><FontAwesomeIcon icon={faGithub}/></a>
+      <a href='https://www.linkedin.com/in/meghan-hein/' target='_blank'><FontAwesomeIcon icon={faLinkedin}/></a>
+      
     </footer>
   )
 }
