29336ceafb75e35c2a1af2ecfed12f49e7236717
Meghan Hein <meghan.hein@gmail.com>
2020-10-29 01:51:51 -0700

    clean up code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faHeart, faCopyright } from '@fortawesome/free-regular-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import './style.scss'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './style.scss';
 
 export default function Footer() {
-
   return (
     <footer>
       <p className='name'>Made with {<FontAwesomeIcon icon={faHeart} />} by Meghan Hein</p>
@@ -16,7 +15,5 @@ export default function Footer() {
         <a href='https://www.linkedin.com/in/meghan-hein/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin}/></a>
       </div>
     </footer>
-  )
-}
-
-{/* <p>Created by <a href='https://resume.creddle.io/resume/hq343p8820e' target='_blank' rel='noreferrer'>Meghan Hein</a></p> */}
+  );
+};
