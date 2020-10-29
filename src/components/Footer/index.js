4f0c5a7de2de0bf363137f5ac2bb70821fc738b1
Meghan Hein <meghan.hein@gmail.com>
2020-10-29 01:50:24 -0700

    adjust footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faCopyright } from '@fortawesome/free-regular-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './style.scss'
 
 export default function Footer() {
+
   return (
     <footer>
-      <p>Created with <FontAwesomeIcon icon={faHeart}/> <a href='https://resume.creddle.io/resume/hq343p8820e' target='_blank'>by Meghan Hein</a></p>
-      <a href='https://github.com/meghein/tandem-quizzes' target='_blank'><FontAwesomeIcon icon={faGithub}/></a>
-      <a href='https://www.linkedin.com/in/meghan-hein/' target='_blank'><FontAwesomeIcon icon={faLinkedin}/></a>
-      
+      <p className='name'>Made with {<FontAwesomeIcon icon={faHeart} />} by Meghan Hein</p>
+      <div className='links'>
+        <a href='mailto:meghan.hein@gmail.com'><FontAwesomeIcon icon={faEnvelope}/></a>
+        <a href='https://github.com/meghein/tandem-quizzes' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub}/></a>
+        <a href='https://www.linkedin.com/in/meghan-hein/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin}/></a>
+      </div>
     </footer>
   )
 }
+
+{/* <p>Created by <a href='https://resume.creddle.io/resume/hq343p8820e' target='_blank' rel='noreferrer'>Meghan Hein</a></p> */}
